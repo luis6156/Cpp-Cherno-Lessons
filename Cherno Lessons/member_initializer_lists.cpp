6840085f716488dcbc15f56f457620c833a2de69
Florian Luis Micu <miculuis1@gmail.com>
2022-07-11 21:38:22 +0300

    added member initializer lists

--- Cherno Lessons/member_initializer_lists.cpp
+++ Cherno Lessons/member_initializer_lists.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 
-int main() { return 0; }
+class Entity {
+   private:
+    std::string m_Name;
+
+   public:
+    // Creates two strings (one default and another with Unknown)
+    Entity() { m_Name = "Unknown"; }
+
+    Entity(const std::string& name) { m_Name = name; }
+
+    // Better (initializes directly instead of assigning)
+    // The order is exactly like you declare the members
+    // Entity() : m_Name("Unknown") {}
+
+    const std::string& GetName() const { return m_Name; }
+};
+
+int main() {
+    Entity e;
+    std::cout << e.GetName() << '\n';
+
+    Entity e1("Cherno");
+    std::cout << e1.getName() << '\n';
+
+    return 0;
+}
